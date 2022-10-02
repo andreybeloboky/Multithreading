a164e411d1f2f4b4c83a8ae836256f94c8bbd079
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-10-02 16:08:34 +0300

    Add deadlock to Deadlock task

--- src/main/java/by/beloboky/deadlock/Deadlock.java
+++ src/main/java/by/beloboky/deadlock/Deadlock.java
@@ -2,10 +2,12 @@ package by.beloboky.deadlock;
 
 public class Deadlock {
     public static void main(String[] args) {
-        Cat cat = new Cat("Alisa");
-        Cat cat2 = new Cat("Wystrik");
-        Thread t1 = new Thread(() -> cat.methodOne(cat2));
-        Thread t2 = new Thread(() -> cat2.methodOne(cat));
+        CatOne cat = new CatOne("Alisa");
+        CatTwo cat2 = new CatTwo("Wystrik");
+        cat.catTwo = cat2;
+        cat2.catOne = cat;
+        Thread t1 = new Thread(cat::methodOne);
+        Thread t2 = new Thread(cat2::methodOne);
         t1.start();
         t2.start();
     }
